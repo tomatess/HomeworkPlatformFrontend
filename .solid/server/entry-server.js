c5c4f650c8ce5537d64d74aa3443afc26b222007
Azur冰弦 <973562770@qq.com>
2023-09-16 01:09:08 +0800

    added ignore file and lock

---
+++ .solid/server/entry-server.js
@@ -0,0 +1,521 @@
+import { ssr } from 'solid-js/web';
+import { sharedConfig } from 'solid-js';
+
+const FETCH_EVENT = "$FETCH";
+
+const XSolidStartLocationHeader = "x-solidstart-location";
+const LocationHeader = "Location";
+const ContentTypeHeader = "content-type";
+const XSolidStartResponseTypeHeader = "x-solidstart-response-type";
+const XSolidStartContentTypeHeader = "x-solidstart-content-type";
+const XSolidStartOrigin = "x-solidstart-origin";
+const JSONResponseType = "application/json";
+const redirectStatusCodes = /* @__PURE__ */ new Set([204, 301, 302, 303, 307, 308]);
+function isRedirectResponse(response) {
+  return response && response instanceof Response && redirectStatusCodes.has(response.status);
+}
+
+class ServerError extends Error {
+  constructor(message, {
+    status,
+    stack
+  } = {}) {
+    super(message);
+    this.name = "ServerError";
+    this.status = status || 400;
+    if (stack) {
+      this.stack = stack;
+    }
+  }
+}
+class FormError extends ServerError {
+  constructor(message, {
+    fieldErrors = {},
+    form,
+    fields,
+    stack
+  } = {}) {
+    super(message, {
+      stack
+    });
+    this.formError = message;
+    this.name = "FormError";
+    this.fields = fields || Object.fromEntries(typeof form !== "undefined" ? form.entries() : []) || {};
+    this.fieldErrors = fieldErrors;
+  }
+}
+
+const root = '';
+
+const rootData = Object.values(/* #__PURE__ */ Object.assign({
+
+}))[0];
+rootData ? rootData.default : undefined;
+
+/** Function responsible for listening for streamed [operations]{@link Operation}. */
+
+/** Input parameters for to an Exchange factory function. */
+
+/** Function responsible for receiving an observable [operation]{@link Operation} and returning a [result]{@link OperationResult}. */
+
+/** This composes an array of Exchanges into a single ExchangeIO function */
+const composeMiddleware = exchanges => ({
+  forward
+}) => exchanges.reduceRight((forward, exchange) => exchange({
+  forward
+}), forward);
+function createHandler(...exchanges) {
+  const exchange = composeMiddleware(exchanges);
+  return async event => {
+    return await exchange({
+      forward: async op => {
+        return new Response(null, {
+          status: 404
+        });
+      }
+    })(event);
+  };
+}
+ssr("<!DOCTYPE html>");
+
+function getRouteMatches(routes, path, method) {
+  const segments = path.split("/").filter(Boolean);
+  routeLoop:
+    for (const route of routes) {
+      const matchSegments = route.matchSegments;
+      if (segments.length < matchSegments.length || !route.wildcard && segments.length > matchSegments.length) {
+        continue;
+      }
+      for (let index = 0; index < matchSegments.length; index++) {
+        const match = matchSegments[index];
+        if (!match) {
+          continue;
+        }
+        if (segments[index] !== match) {
+          continue routeLoop;
+        }
+      }
+      const handler = route[method];
+      if (handler === "skip" || handler === void 0) {
+        return;
+      }
+      const params = {};
+      for (const { type, name, index } of route.params) {
+        if (type === ":") {
+          params[name] = segments[index];
+        } else {
+          params[name] = segments.slice(index).join("/");
+        }
+      }
+      return { handler, params };
+    }
+}
+
+let apiRoutes$1;
+const registerApiRoutes = (routes) => {
+  apiRoutes$1 = routes;
+};
+async function internalFetch(route, init, env = {}, locals = {}) {
+  if (route.startsWith("http")) {
+    return await fetch(route, init);
+  }
+  let url = new URL(route, "http://internal");
+  const request = new Request(url.href, init);
+  const handler = getRouteMatches(apiRoutes$1, url.pathname, request.method.toUpperCase());
+  if (!handler) {
+    throw new Error(`No handler found for ${request.method} ${request.url}`);
+  }
+  let apiEvent = Object.freeze({
+    request,
+    params: handler.params,
+    clientAddress: "127.0.0.1",
+    env,
+    locals,
+    $type: FETCH_EVENT,
+    fetch: internalFetch
+  });
+  const response = await handler.handler(apiEvent);
+  return response;
+}
+
+const api = [
+  {
+    GET: "skip",
+    path: "/*404"
+  },
+  {
+    GET: "skip",
+    path: "/"
+  }
+];
+function expandOptionals(pattern) {
+  let match = /(\/?\:[^\/]+)\?/.exec(pattern);
+  if (!match)
+    return [pattern];
+  let prefix = pattern.slice(0, match.index);
+  let suffix = pattern.slice(match.index + match[0].length);
+  const prefixes = [prefix, prefix += match[1]];
+  while (match = /^(\/\:[^\/]+)\?/.exec(suffix)) {
+    prefixes.push(prefix += match[1]);
+    suffix = suffix.slice(match[0].length);
+  }
+  return expandOptionals(suffix).reduce(
+    (results, expansion) => [...results, ...prefixes.map((p) => p + expansion)],
+    []
+  );
+}
+function routeToMatchRoute(route) {
+  const segments = route.path.split("/").filter(Boolean);
+  const params = [];
+  const matchSegments = [];
+  let score = 0;
+  let wildcard = false;
+  for (const [index, segment] of segments.entries()) {
+    if (segment[0] === ":") {
+      const name = segment.slice(1);
+      score += 3;
+      params.push({
+        type: ":",
+        name,
+        index
+      });
+      matchSegments.push(null);
+    } else if (segment[0] === "*") {
+      score -= 1;
+      params.push({
+        type: "*",
+        name: segment.slice(1),
+        index
+      });
+      wildcard = true;
+    } else {
+      score += 4;
+      matchSegments.push(segment);
+    }
+  }
+  return {
+    ...route,
+    score,
+    params,
+    matchSegments,
+    wildcard
+  };
+}
+const allRoutes = api.flatMap((route) => {
+  const paths = expandOptionals(route.path);
+  return paths.map((path) => ({ ...route, path }));
+}).map(routeToMatchRoute).sort((a, b) => b.score - a.score);
+registerApiRoutes(allRoutes);
+function getApiHandler(url, method) {
+  return getRouteMatches(allRoutes, url.pathname, method.toUpperCase());
+}
+
+const apiRoutes = ({ forward }) => {
+  return async (event) => {
+    let apiHandler = getApiHandler(new URL(event.request.url), event.request.method);
+    if (apiHandler) {
+      let apiEvent = Object.freeze({
+        request: event.request,
+        httpServer: event.httpServer,
+        clientAddress: event.clientAddress,
+        locals: event.locals,
+        params: apiHandler.params,
+        env: event.env,
+        $type: FETCH_EVENT,
+        fetch: event.fetch
+      });
+      try {
+        return await apiHandler.handler(apiEvent);
+      } catch (error) {
+        if (error instanceof Response) {
+          return error;
+        }
+        return new Response(
+          JSON.stringify({
+            error: error.message
+          }),
+          {
+            headers: {
+              "Content-Type": "application/json"
+            },
+            status: 500
+          }
+        );
+      }
+    }
+    return await forward(event);
+  };
+};
+
+const server$ = (_fn) => {
+  throw new Error("Should be compiled away");
+};
+async function parseRequest(event) {
+  let request = event.request;
+  let contentType = request.headers.get(ContentTypeHeader);
+  let name = new URL(request.url).pathname, args = [];
+  if (contentType) {
+    if (contentType === JSONResponseType) {
+      let text = await request.text();
+      try {
+        args = JSON.parse(
+          text,
+          (key, value) => {
+            if (!value) {
+              return value;
+            }
+            if (value.$type === "fetch_event") {
+              return event;
+            }
+            return value;
+          }
+        );
+      } catch (e) {
+        throw new Error(`Error parsing request body: ${text}`);
+      }
+    } else if (contentType.includes("form")) {
+      let formData = await request.clone().formData();
+      args = [formData, event];
+    }
+  }
+  return [name, args];
+}
+function respondWith(request, data, responseType) {
+  if (data instanceof Response) {
+    if (isRedirectResponse(data) && request.headers.get(XSolidStartOrigin) === "client") {
+      let headers = new Headers(data.headers);
+      headers.set(XSolidStartOrigin, "server");
+      headers.set(XSolidStartLocationHeader, data.headers.get(LocationHeader) ?? "/");
+      headers.set(XSolidStartResponseTypeHeader, responseType);
+      headers.set(XSolidStartContentTypeHeader, "response");
+      return new Response(null, {
+        status: 204,
+        statusText: "Redirected",
+        headers
+      });
+    } else if (data.status === 101) {
+      return data;
+    } else {
+      let headers = new Headers(data.headers);
+      headers.set(XSolidStartOrigin, "server");
+      headers.set(XSolidStartResponseTypeHeader, responseType);
+      headers.set(XSolidStartContentTypeHeader, "response");
+      return new Response(data.body, {
+        status: data.status,
+        statusText: data.statusText,
+        headers
+      });
+    }
+  } else if (data instanceof FormError) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          message: data.message,
+          stack: "",
+          formError: data.formError,
+          fields: data.fields,
+          fieldErrors: data.fieldErrors
+        }
+      }),
+      {
+        status: 400,
+        headers: {
+          [XSolidStartResponseTypeHeader]: responseType,
+          [XSolidStartContentTypeHeader]: "form-error"
+        }
+      }
+    );
+  } else if (data instanceof ServerError) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          message: data.message,
+          stack: ""
+        }
+      }),
+      {
+        status: data.status,
+        headers: {
+          [XSolidStartResponseTypeHeader]: responseType,
+          [XSolidStartContentTypeHeader]: "server-error"
+        }
+      }
+    );
+  } else if (data instanceof Error) {
+    console.error(data);
+    return new Response(
+      JSON.stringify({
+        error: {
+          message: "Internal Server Error",
+          stack: "",
+          status: data.status
+        }
+      }),
+      {
+        status: data.status || 500,
+        headers: {
+          [XSolidStartResponseTypeHeader]: responseType,
+          [XSolidStartContentTypeHeader]: "error"
+        }
+      }
+    );
+  } else if (typeof data === "object" || typeof data === "string" || typeof data === "number" || typeof data === "boolean") {
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        [ContentTypeHeader]: "application/json",
+        [XSolidStartResponseTypeHeader]: responseType,
+        [XSolidStartContentTypeHeader]: "json"
+      }
+    });
+  }
+  return new Response("null", {
+    status: 200,
+    headers: {
+      [ContentTypeHeader]: "application/json",
+      [XSolidStartContentTypeHeader]: "json",
+      [XSolidStartResponseTypeHeader]: responseType
+    }
+  });
+}
+async function handleServerRequest(event) {
+  const url = new URL(event.request.url);
+  if (server$.hasHandler(url.pathname)) {
+    try {
+      let [name, args] = await parseRequest(event);
+      let handler = server$.getHandler(name);
+      if (!handler) {
+        throw {
+          status: 404,
+          message: "Handler Not Found for " + name
+        };
+      }
+      const data = await handler.call(event, ...Array.isArray(args) ? args : [args]);
+      return respondWith(event.request, data, "return");
+    } catch (error) {
+      return respondWith(event.request, error, "throw");
+    }
+  }
+  return null;
+}
+const handlers = /* @__PURE__ */ new Map();
+server$.createHandler = (_fn, hash, serverResource) => {
+  let fn = function(...args) {
+    let ctx;
+    if (typeof this === "object") {
+      ctx = this;
+    } else if (sharedConfig.context && sharedConfig.context.requestContext) {
+      ctx = sharedConfig.context.requestContext;
+    } else {
+      ctx = {
+        request: new URL(hash, `http://localhost:${process.env.PORT ?? 3e3}`).href,
+        responseHeaders: new Headers()
+      };
+    }
+    const execute = async () => {
+      try {
+        return serverResource ? _fn.call(ctx, args[0], ctx) : _fn.call(ctx, ...args);
+      } catch (e) {
+        if (e instanceof Error && /[A-Za-z]+ is not defined/.test(e.message)) {
+          const error = new Error(
+            e.message + "\n You probably are using a variable defined in a closure in your server function."
+          );
+          error.stack = e.stack;
+          throw error;
+        }
+        throw e;
+      }
+    };
+    return execute();
+  };
+  fn.url = hash;
+  fn.action = function(...args) {
+    return fn.call(this, ...args);
+  };
+  return fn;
+};
+server$.registerHandler = function(route, handler) {
+  handlers.set(route, handler);
+};
+server$.getHandler = function(route) {
+  return handlers.get(route);
+};
+server$.hasHandler = function(route) {
+  return handlers.has(route);
+};
+server$.fetch = internalFetch;
+
+const inlineServerFunctions = ({ forward }) => {
+  return async (event) => {
+    const url = new URL(event.request.url);
+    if (server$.hasHandler(url.pathname)) {
+      let contentType = event.request.headers.get(ContentTypeHeader);
+      let origin = event.request.headers.get(XSolidStartOrigin);
+      let formRequestBody;
+      if (contentType != null && contentType.includes("form") && !(origin != null && origin.includes("client"))) {
+        let [read1, read2] = event.request.body.tee();
+        formRequestBody = new Request(event.request.url, {
+          body: read2,
+          headers: event.request.headers,
+          method: event.request.method,
+          duplex: "half"
+        });
+        event.request = new Request(event.request.url, {
+          body: read1,
+          headers: event.request.headers,
+          method: event.request.method,
+          duplex: "half"
+        });
+      }
+      let serverFunctionEvent = Object.freeze({
+        request: event.request,
+        clientAddress: event.clientAddress,
+        locals: event.locals,
+        fetch: event.fetch,
+        $type: FETCH_EVENT,
+        env: event.env
+      });
+      const serverResponse = await handleServerRequest(serverFunctionEvent);
+      if (serverResponse) {
+        let responseContentType = serverResponse.headers.get(XSolidStartContentTypeHeader);
+        if (formRequestBody && responseContentType !== null && responseContentType.includes("error")) {
+          const formData = await formRequestBody.formData();
+          let entries = [...formData.entries()];
+          return new Response(null, {
+            status: 302,
+            headers: {
+              Location: new URL(event.request.headers.get("referer")).pathname + "?form=" + encodeURIComponent(
+                JSON.stringify({
+                  url: url.pathname,
+                  entries,
+                  ...await serverResponse.json()
+                })
+              )
+            }
+          });
+        }
+        return serverResponse;
+      }
+    }
+    const response = await forward(event);
+    return response;
+  };
+};
+
+function renderAsync$1(fn, options) {
+  return () => apiRoutes({
+    forward: inlineServerFunctions({
+      async forward(event) {
+        {
+          return await event.env.getStaticHTML?.("/index") ?? new Response("Not Found", { status: 404 });
+        }
+      }
+    })
+  });
+}
+
+const renderAsync = (fn, options) => composeMiddleware([apiRoutes, inlineServerFunctions, renderAsync$1()]);
+
+const entryServer = createHandler(renderAsync());
+
+export { entryServer as default };
